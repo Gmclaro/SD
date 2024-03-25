8b443705cf2f4b4f6aa5e5088d49f76af5348f16
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2024-03-25 22:49:53 +0000

    changes

--- GameOfRope/run.sh
+++ GameOfRope/run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 export CLASSPATH=./commonInfra/genclass.jar
 
+sleep 0.1
+
 rm ./*/*.class
+
+sleep 0.1
 
 javac ./*/*.java
 
+sleep 0.1
+
 java main.GameOfRope
+
+sleep 0.1
 
 cat out
