64d184d01088a18d06b432d3d833e140a6726779
Gmclaro <gui@ua.pt>
2024-03-26 17:59:52 +0000

    callcontestants and seatdown

--- GameOfRope/sharedRegions/ContestantBench.java
+++ GameOfRope/sharedRegions/ContestantBench.java
@@ -9,9 +9,6 @@ import entities.*;
 
 public class ContestantBench {
 
-    /**
-     * General Repository of Information
-     */
     private final GeneralRepository repo;
 
     private final Contestant[][] contestants;
@@ -23,24 +20,40 @@ public class ContestantBench {
         playgroundQueue = new MemFIFO[2];
     }
 
-
-    public synchronized void callContestants(int[] selected,int team) throws MemException {
-
+    public synchronized void callContestants(int[] selected, int team) throws MemException {
         // TODO: implement callContestants
-        for(int contestantID:selected){
+        for (int contestantID : selected) {
             playgroundQueue[team].write(contestantID);
         }
         notifyAll();
     }
 
     public synchronized void followCoachAdvice() {
-        // TODO: implement followCoachAdvice
-                
+        // try {
+        //     // Wait until there are contestants in the playground queue
+        //     while (playgroundQueue[Coach.team].isEmpty()) {
+        //         wait();
+        //     }
+
+        //     // Remove a contestant from the playground queue
+        //     int contestantID = playgroundQueue[Coach.team].read();
+
+        //     // Perform actions based on coach's advice (if needed)
+        //     // For example, update contestant's state or perform specific actions
+
+        //     // Notify waiting threads (if any)
+        //     notifyAll();
+        // } catch (InterruptedException e) {
+        //     // Handle InterruptedException if needed
+        //     e.printStackTrace();
+        // }
     }
 
     public synchronized void seatDown(int id, int team) {
-        // TODO: implement seatDown
+
+        contestants[team][id] = (Contestant) Thread.currentThread();
+        contestants[team][id].setEntityState(ContestantState.SEAT_AT_THE_BENCH);
+        notifyAll();
     }
-    
 
 }
