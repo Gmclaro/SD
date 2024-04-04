e4941fab2207fc07985d81553010e31fceee70c9
Gmclaro <gui@ua.pt>
2024-04-04 12:37:18 +0100

    fix

--- GameOfRope/entities/CoachState.java
+++ GameOfRope/entities/CoachState.java
@@ -5,15 +5,15 @@ package entities;
  * 
  */
 public class CoachState {
-    /*
+    /**
      * The coach is waiting for the referee command
      */
     public static final int WAIT_FOR_REFEREE_COMMAND = 0;
-    /*
+    /**
      * The coach is assembling the team
      */
     public static final int ASSEMBLE_TEAM = 1;
-    /*
+    /**
      * The coach is watching the trial
      */
     public static final int WATCH_TRIAL = 2;
