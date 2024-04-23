acdfcae6dd3024abc16dd3530a039fa6fc0c5b0f
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2024-04-23 17:20:36 +0100

    updating run

--- GameOfRope/run.sh
+++ GameOfRope/run.sh
@@ -23,16 +23,16 @@ javac ./*/*.java
 
 echo "java main.GameOfRope"
 
-#for ((i = 1; i <= $NUMBER_OF_MATCH; i++)); do
-#    echo -e "\033[34mNEW RUN $i\033[0m"
-#    java main.GameOfRope
-#    sleep 0.1
-#done
+for ((i = 1; i <= $NUMBER_OF_MATCH; i++)); do
+   echo -e "\033[34mNEW RUN $i\033[0m"
+   java main.GameOfRope
+   sleep 0.1
+done
 
 
-java main.GameOfRope
+#java main.GameOfRope
 
 if [ $? -eq 0 ]; then
     sleep 0.1
-    batcat log
+    bat log
 fi
