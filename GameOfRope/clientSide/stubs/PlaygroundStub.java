6c762954a266085b2b5d1dce14035c34c7d2de8f
Gmclaro <gui@ua.pt>
2024-05-03 17:30:19 +0100

    TODOS for the playground

--- GameOfRope/clientSide/stubs/PlaygroundStub.java
+++ GameOfRope/clientSide/stubs/PlaygroundStub.java
@@ -5,7 +5,7 @@ import commonInfra.Message;
 import commonInfra.MessageType;
 
 public class PlaygroundStub {
-        /**
+    /**
      * Name of the platform where is located the Playground server
      */
     private String serverHostName;
@@ -18,10 +18,10 @@ public class PlaygroundStub {
      * Instantiation of a Playground stub
      * 
      * @param hostName
-     *            name of the platform where is located the playground
-     *            server
+     *                 name of the platform where is located the playground
+     *                 server
      * @param port
-     *            port number for listening to service requests
+     *                 port number for listening to service requests
      */
 
     public PlaygroundStub(String hostname, int port) {
@@ -29,25 +29,78 @@ public class PlaygroundStub {
         serverPortNumb = port;
     }
 
-        public void shutdown() {
+    // TODO: followCoachAdvice
+    public void followCoachAdvice(int team) {
+
+    }
+
+    // TODO: waitforFollowCoachAdvice
+    public void waitForFollowCoachAdvice(int team) {
+
+    }
+
+    // TODO: startTrial
+
+    public void startTrial() {
+    }
+
+    // TODO: waitforStartTrial
+    public void waitForStartTrial(int team, int id) {
+    }
+
+    // TODO: getReady
+    public void getReady(int team, int id) {
+    }
+
+    // TODO: amDone
+
+    public void amDone() {
+    }
+
+    // TODO : waitForAmDone
+
+    public void waitForAmDone() {
+    }
+
+    // TODO: assertTrialDecision
+
+    public void assertTrialDecision() {
+    }
+
+    // TODO: waitForassertTrialDecision
+
+    public void waitForAssertTrialDecision(int team, int id) {
+    }
+
+    // TODO: waitForAssertTrialDecision
+
+    public void waitForAssertTrialDecision(int team) {
+    }
+
+    // TODO: declareGameWinner
+
+    public void declareGameWinner() {
+    }
+
+    public void shutdown() {
         ClientCom com;
-        Message inMessage,outMessage;
+        Message inMessage, outMessage;
 
         com = new ClientCom(serverHostName, serverPortNumb);
 
-        while(!com.open()) {
+        while (!com.open()) {
             try {
-                Thread.currentThread ().sleep ((long) (10));
+                Thread.currentThread().sleep((long) (10));
+            } catch (InterruptedException e) {
             }
-            catch (InterruptedException e) {}
         }
 
         outMessage = new Message(MessageType.REQ_PLAYGROUND_SHUTDOWN);
         com.writeObject(outMessage);
         inMessage = (Message) com.readObject();
 
-        if(inMessage.getMsgType() != MessageType.REP_PLAYGROUND_SHUTDOWN){
-            System.out.println("Thread "+ Thread.currentThread().getName()+ ":Invalid message type!");
+        if (inMessage.getMsgType() != MessageType.REP_PLAYGROUND_SHUTDOWN) {
+            System.out.println("Thread " + Thread.currentThread().getName() + ":Invalid message type!");
             System.out.println(inMessage.toString());
             System.exit(1);
         }
--- GameOfRope/serverSide/main/ServerGameOfRopePlayground.java
+++ GameOfRope/serverSide/main/ServerGameOfRopePlayground.java
@@ -1,5 +1,5 @@
 package serverSide.main;
 
 public class ServerGameOfRopePlayground {
-    
+    //TODO: fazer o main para o playgorund
 }
